cf7bb540f6c4c4d810e9f7a867f2561ccf38cfff
FrederikGJ <113090989+FrederikGJ@users.noreply.github.com>
2023-11-15 14:37:48 +0100

    changes to react project

--- appelap/src/App.jsx
+++ appelap/src/App.jsx
@@ -2,34 +2,28 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import yorkshire from 'appelap/src/assets/yorkshire.jpg'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <img src={reactLogo} className="react-logo" alt="react-logo" />
+      <h1>Vite + React</h1>
+      <h2> Det er lidt hacked det her ?  </h2>
+      <div className='textbox'>
+
+        <p> Jeg hedder Jon </p>
+
       </div>
-      <h1>Vite + React + Hund</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h3>Jeg hedder ikke John John  </h3>
+
+
     </>
   )
 }
+
+//
 
 export default App
